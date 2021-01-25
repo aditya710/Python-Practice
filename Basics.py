41ba27f6f4f0de6a3c261b5a89e8b4d0146816d0
Aditya Shelar <adityashelar710@gmail.com>
2021-01-25 18:21:53 +0100

    First program

---
+++ Basics.py
@@ -0,0 +1,8 @@
+age = 20
+name = 'Swaroop'
+
+
+print(name + ' was ' + str(age) +  ' years old when he wrote this book')
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+
+print('Why is {0} playing with that python?'.format(name))
